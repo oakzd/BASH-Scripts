1aa43951ea1b693ba6d26a1dd93d45b08ab6d5d1
zaid <oakzaid@gmail.com>
2022-06-16 15:10:16 -0500

    Added final project file and renamed a few things

---
+++ Part 2 - Adding User Account with random password/add_user_account_with_password.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Script Breakdown: This will add a useraccount with generating a random passwd all on command line
+
+# Has to run with sudo privileges and exit with 1 if it doesnt
+if [[ "${UID}" != 0 ]]
+then
+	echo 'Sorry you are not root exiting...'
+	exit 1
+else
+	: # this mean do "nothing" and keep going
+fi
+
+#Verify that a argument is sent through, then provide a 'man page' usage statement 
+if [[ "${#}" -lt 1 ]]
+then
+	echo "Usage: ${0} USERNAME [COMMENT]... "
+	echo 'Create an account on the local system with username and comment field'
+	exit 1
+else
+	:
+fi
+
+# Make UserName the first argument
+USER_NAME="${1}"
+
+# Shift over 1 to make rest of the arguments the comment field 
+shift 1
+FULL_NAME+="${@}"
+
+# create and add the new user with the supplied info
+useradd -c "${FULL_NAME}" ${USER_NAME}
+
+# Checking exit status of previous executed command to verify and then creates account password automatically
+if [[ "${?}" != 0 ]]
+then
+	echo "Sorry looks like account was not created"
+	exit 1
+else
+	echo "Account created successfully Dislayed below"
+	ACCOUNT_PASSWORD=$(date +%s%N${RANDOM} | sha256sum | head -c13)
+	echo "${USER_NAME}:${ACCOUNT_PASSWORD}" | chpasswd --md5
+	passwd -e ${USER_NAME}
+fi
+
+# Display username,password, and hostname of the account that was created
+echo -e "\nFullname: ${FULL_NAME} \nAccount: ${USER_NAME} \nPassword: ${ACCOUNT_PASSWORD}\nHostname: $(hostname) \nAccount creation completed!\n"
+echo exit 0
